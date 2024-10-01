23978046f147a14657c48b20138832cb0ab24fe1
TimTHsiao <145700032+TimTHsiao@users.noreply.github.com>
2024-10-01 22:28:37 +0000

    forgot ening 0

--- hello.c
+++ hello.c
@@ -3,7 +3,7 @@
 int main() {
   char hello[] = "Hello!"; 
   char hello2[] = { 72, 101, 108, 108, 111, 33, 0 };
-  char binary[] = {43, 32, 56, 120, 1, 7, 10};
+  char binary[] = {43, 32, 56, 120, 1, 7, 10, 0};
   char test_message[] = "This is edited in codespace, then uploaded to ieng6";
 
   puts(hello);
